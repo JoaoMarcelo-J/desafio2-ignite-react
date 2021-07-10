b16a8579bcc35718d12e4deb78d809f29500152a
João Marcelo Vieira <joaonininho.twt@gmail.com>
2021-07-10 12:12:55 -0300

    Enviando Desafio 2 Ignite React

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,55 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+import { MovieCard } from "./MovieCard";
+import { api } from "../services/api";
+import "../styles/content.scss";
+import "../styles/global.scss";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+type ContentProps = {
+  property: {
+    selectedGenre: GenreResponseProps;
+    movies: MovieProps[];
+  };
+};
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.property.selectedGenre.title}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.property.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,42 @@
-export function SideBar() {
+import { Button } from "./Button";
+
+import "../styles/sidebar.scss";
+import "../styles/global.scss";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+type SideBarProps = {
+  property: {
+    genres: GenreResponseProps[];
+    selectedGenreId: number;
+    handleClickButton: (id: number) => void;
+  };
+};
+
+export function SideBar(props: SideBarProps) {
   // Complete aqui
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.property.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.property.handleClickButton(genre.id)}
+            selected={props.property.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
